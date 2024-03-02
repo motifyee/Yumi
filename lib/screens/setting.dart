6c96543f411c40478b477fc2f41c5d75eb60fcdc
abduo ghazaly <abduo.ghazaly@gmail.com>
2024-03-02 10:12:26 +0200

    fix setting index again

--- lib/screens/setting.dart
+++ lib/screens/setting.dart
@@ -48,7 +48,9 @@ class SettingScreen extends StatelessWidget {
                     ),
                     child: Center(
                       child: Text(
-                        state.profile.firstName[0],
+                        state.profile.firstName.isNotEmpty
+                            ? state.profile.firstName[0].toUpperCase()
+                            : '',
                         style: TextStyle(
                           color: ThemeSelector.colors.onPrimary,
                           fontSize: ThemeSelector.fonts.font_18,
