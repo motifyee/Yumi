9232a600cc3f3b0ebd0500f9302a99b2b940c2b2
mrt <iquixe@gmail.com>
2024-05-11 12:14:06 +0300

    fix registeration fixed code bug

--- lib/app/pages/auth/register/repository/signup_service.dart
+++ lib/app/pages/auth/register/repository/signup_service.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/cupertino.dart';
 import 'package:yumi/app/pages/auth/register/model/registeration.dart';
 import 'package:yumi/statics/api_statics.dart';
+import 'package:yumi/statics/code_generator.dart';
 
 class SignUpService {
   static Future<dynamic> signUp(
       {required RegisterationForm signup,
       required BuildContext context}) async {
-    return await DioClient.simpleDio(context).post(
-        ApiKeys.getApiKeyString(apiKey: ApiKeys.signup),
-        data: signup.toJson());
+    return await DioClient.simpleDio(context)
+        .post(ApiKeys.getApiKeyString(apiKey: ApiKeys.signup),
+            data: signup
+                .copyWith(
+                  code: CodeGenerator.getRandomCode(),
+                )
+                .toJson());
   }
 }
