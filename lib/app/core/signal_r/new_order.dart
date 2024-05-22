1d0699a2cc0bf0df5c66f0347a2a312f7b0d48e1
abduo ghazaly <abduo.ghazaly@gmail.com>
2024-05-22 12:57:48 +0300

    new order signalr

--- lib/app/core/signal_r/new_order.dart
+++ lib/app/core/signal_r/new_order.dart
@@ -8,3 +8,5 @@ class NewOrderReceivedSignalR {
     });
   }
 }
+
+// {invoiceId: 300919, chef_ID: c2d02a9b-171b-4517-8e16-5ca0726f2bf5, driver_ID: null, is_Pickup: true, is_Delivery: false, is_Preorder: false}
