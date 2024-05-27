8b3b86911b3f43a812e22282c0317cba7ce81181
abduo ghazaly <abduo.ghazaly@gmail.com>
2024-05-27 12:11:09 +0300

    all : signal r reconnect time

--- lib/app/core/setup/signalr.dart
+++ lib/app/core/setup/signalr.dart
@@ -66,8 +66,18 @@ class Signalr {
     hubConnection = HubConnectionBuilder()
         .withUrl("$baseUrl/$hubName", options: httpOptions)
         .configureLogging(hubLogger)
-        .withAutomaticReconnect(
-            retryDelays: [1000, 2000, 4000, 8000, 15000, 30000])
+        .withAutomaticReconnect(retryDelays: [
+          0,
+          1000,
+          2000,
+          4000,
+          8000,
+          15000,
+          30000,
+          40000,
+          50000,
+          60000,
+        ])
         .withHubProtocol(JsonHubProtocol())
         .build();
 
@@ -153,7 +163,8 @@ class HttpOverrideCertificateVerificationInDev extends HttpOverrides {
 }
 
 void onclose({Exception? error}) {
-  debugPrint("SignalR closed");
+  debugPrint("SignalR closed ");
+  debugPrint(error.toString() ?? ' ');
 }
 
 void onreconnecting({Exception? error}) {
