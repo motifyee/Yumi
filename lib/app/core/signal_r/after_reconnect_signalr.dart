1331c5af2fa28a57f14959f6f153810c3df6cf63
abduo ghazaly <abduo.ghazaly@gmail.com>
2024-06-10 15:44:21 +0300

    chef - driver : on reconnect request status

---
+++ lib/app/core/signal_r/after_reconnect_signalr.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:yumi/bloc/user/user_bloc.dart';
+import 'package:yumi/global.dart';
+import 'package:yumi/service/chef_service.dart';
+
+class AfterReconnectSignalr {
+  static reconnectedCall() async {
+    if (G.isCustomerApp) return;
+    await ChefService.getChefStatus(
+            accountId: G.context.read<UserBloc>().state.user.id)
+        .then((value) => G.context.read<UserBloc>().add(UserFromJsonEvent(
+            user: G.context
+                .read<UserBloc>()
+                .state
+                .user
+                .copyWith(status: value.data['statusWork'])
+                .toJson())));
+  }
+}
