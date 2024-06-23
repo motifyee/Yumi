9e4e1aeb7c9e5388fb0387db409cc9b8a8d76026
mrt <iquixe@gmail.com>
2024-06-23 12:14:25 +0300

    create FailureX extension

--- lib/core/failures.dart
+++ lib/core/failures.dart
@@ -1,8 +1,31 @@
 import 'package:equatable/equatable.dart';
+import 'package:yumi/core/exceptions.dart';
 
 sealed class Failure {
   Object? error;
   StackTrace? stackTrace;
+}
+
+extension FailureX on Failure {
+  bool get isNetworkFailure => this is NetworkFailure;
+
+  bool get isServerFailure => this is ServerFailure;
+
+  bool get isGenericFailure => this is GenericFailure;
+
+  bool get isCacheFailure => this is CacheFailure;
+
+  bool get isSignalrFailure => this is SignalrFailure;
+
+  static Failure fromException(Object? error, StackTrace? stackTrace) {
+    return switch (error as CException) {
+      ServerException() => ServerFailure(error, stackTrace),
+      NetworkException() => NetworkFailure(error, stackTrace),
+      CacheException() => CacheFailure(error, stackTrace),
+      GenericException() => GenericFailure(error, stackTrace),
+      SignalRException() => SignalrFailure(error, stackTrace),
+    } as Failure;
+  }
 }
 
 // -----------------------------------------------------------------------------
