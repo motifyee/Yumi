d353be456b1179051d7ce8076e3b00f2a2e84a52
gh <abduo.ghazaly@gmail.com>
2024-06-30 11:58:51 +0300

    refactor meal form  cubit

---
+++ lib/app/pages/menu/cubit/meal_form/meal_form_cubit.g.dart
@@ -0,0 +1,17 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'meal_form_cubit.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$MealFormStateImpl _$$MealFormStateImplFromJson(Map<String, dynamic> json) =>
+    _$MealFormStateImpl(
+      mealModel: Meal.fromJson(json['mealModel'] as Map<String, dynamic>),
+    );
+
+Map<String, dynamic> _$$MealFormStateImplToJson(_$MealFormStateImpl instance) =>
+    <String, dynamic>{
+      'mealModel': instance.mealModel,
+    };
