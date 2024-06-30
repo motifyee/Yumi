37d5ff7cf01f71a169795c55d20f3f909c5b2599
gh <abduo.ghazaly@gmail.com>
2024-06-30 11:14:59 +0300

    refactor ingredients form to cubit

---
+++ lib/app/pages/menu/cubit/ingredient_form/ingredients_form_cubit.g.dart
@@ -0,0 +1,21 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'ingredients_form_cubit.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$IngredientsStateImpl _$$IngredientsStateImplFromJson(
+        Map<String, dynamic> json) =>
+    _$IngredientsStateImpl(
+      ingredientsModelList: (json['ingredientsModelList'] as List<dynamic>)
+          .map((e) => Ingredients.fromJson(e as Map<String, dynamic>))
+          .toList(),
+    );
+
+Map<String, dynamic> _$$IngredientsStateImplToJson(
+        _$IngredientsStateImpl instance) =>
+    <String, dynamic>{
+      'ingredientsModelList': instance.ingredientsModelList,
+    };
