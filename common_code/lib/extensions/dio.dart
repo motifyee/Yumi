644e877eb693ee7fc41213af8c69adcf3192276f
mrt <iquixe@gmail.com>
2024-08-21 11:07:10 +0300

    DioExceptionX: fix errorMsg in case response.data is null

--- common_code/lib/extensions/dio.dart
+++ common_code/lib/extensions/dio.dart
@@ -1,9 +1,12 @@
-import 'package:dio/dio.dart';
+import 'package:dependencies/dependencies.dart';
 
 extension DioExceptionX on DioException {
   String? get errorMsg {
-    if (response?.data is String) return response!.data;
+    if (response == null) return message;
 
-    return response?.data['message'];
+    if (response!.data == null) return null;
+    if (response!.data is String) return response!.data;
+
+    return response!.data['message'];
   }
 }
