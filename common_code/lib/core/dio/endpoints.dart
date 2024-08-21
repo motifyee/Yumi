95043bd2fb52ad1b709d52d2fd14f97a56d54dad
mrt <iquixe@gmail.com>
2024-08-21 11:02:18 +0300

    endpoints: make a singleton for testability

--- common_code/lib/core/dio/endpoints.dart
+++ common_code/lib/core/dio/endpoints.dart
@@ -8,7 +8,8 @@ String originApi = '';
 
 class BaseUrl {
   static Future<void> load() async {
-    final value = await LocalStorage.sharedRef.getValue(LocalStorage.domainName);
+    final value =
+        await LocalStorage.sharedRef.getValue(LocalStorage.domainName);
 
     originApi = value ?? defaultOriginApi;
     APIClient.baseUrl = originApi;
@@ -24,12 +25,17 @@ class BaseUrl {
   }
 }
 
-class EndPoints {
-  static String getApiKeyString({required String apiKey}) {
+class Endpoints {
+  factory Endpoints() => _inner;
+  static final Endpoints _inner = Endpoints._init();
+  Endpoints._init();
+
+  static String getApiKey(String apiKey) {
     return apiKey.replaceAll("_", GlobalContext().appConfig.appTitle);
   }
 
-  static String actionApiKeyString({required String apiKey, required String id}) {
+  static String actionApiKeyString(
+      {required String apiKey, required String id}) {
     return apiKey.replaceAll("_", id);
   }
 
@@ -39,104 +45,130 @@ class EndPoints {
   ///
 
   /// stripe
-  static String stripeApi = 'https://api.stripe.com/v1';
-  static String stripePaymentIntent = '/payment_intents';
+  String stripeApi = 'https://api.stripe.com/v1';
+  String stripePaymentIntent = '/payment_intents';
 
-  static String login = '/accounts/_/login';
-  static String signup = '/accounts/_/register';
-  static String profile = '/accounts/_';
-  static String info = '/_';
-  static String address = '/accounts/address';
-  static String userStatus = '/accounts/status';
-  static String notifications = '/notifications';
-  static String transactions = '/account/transactions';
-  static String wallet = '/account/balance';
+  final String _login = '/accounts/_/login';
+  String get login => getApiKey(_login);
+  final String _signup = '/accounts/_/register';
+  String get signup => getApiKey(_signup);
+  final String _profile = '/accounts/_';
+  String get profile => getApiKey(_profile);
+  final String _info = '/_';
+  String get info => getApiKey(_info);
+  String address = '/accounts/address';
+  String userStatus = '/accounts/status';
+  String notifications = '/notifications';
+  String transactions = '/account/transactions';
+  String wallet = '/account/balance';
 
-  static String categories = '/categories';
-  static String categoriesForCustomer = '/meals/categories';
-  static String categoriesForChef = '/meals/categories/chef';
-  static String categoriesForCustomerByChefId = '/meals/categories/chef';
-  static String ingredient = '/Product/Ingredients';
+  String categories = '/categories';
+  String categoriesForCustomer = '/meals/categories';
+  String categoriesForChef = '/meals/categories/chef';
+  String categoriesForCustomerByChefId = '/meals/categories/chef';
+  String ingredient = '/Product/Ingredients';
 
-  static String meal = '/Product/_/meal'; // _ => chefs
-  static String getMeal = '/meals';
-  static String getMealById = '/product/meal/_'; // _ => id
-  static String getMealByCategory = '/meals/category';
-  static String getMealByChef = '/meals/chef';
-  static String getMealByChefByCategory = '/meals/category/chef';
-  static String favoriteMeals = '/meals/favorite';
-  static String mealCalories = '/Product/_/calories';
+  final String _meal = '/Product/_/meal'; //  => chefs
+  String get meal => getApiKey(_meal);
+  String getMeal = '/meals';
+  final String _getMealById = '/product/meal/_'; //  => id
+  String get getMealById => getApiKey(_getMealById);
+  String getMealByCategory = '/meals/category';
+  String getMealByChef = '/meals/chef';
+  String getMealByChefByCategory = '/meals/category/chef';
+  String favoriteMeals = '/meals/favorite';
+  final String _mealCalories = '/Product/_/calories';
+  String get mealCalories => getApiKey(_mealCalories);
 
-  static String chefs = '/accounts/chefs';
-  static String chefsOrder = '/accounts/chefs/Order';
-  static String chefsPreOrder = '/accounts/chefs/PreOrder';
-  static String favoriteChefs = '/accounts/favorite';
-  static String favoriteChef = '/accounts/favorit';
-  static String chefStatus = '/accounts/WorkStatus';
+  String chefs = '/accounts/chefs';
+  String chefsOrder = '/accounts/chefs/Order';
+  String chefsPreOrder = '/accounts/chefs/PreOrder';
+  String favoriteChefs = '/accounts/favorite';
+  String favoriteChef = '/accounts/favorit';
+  String chefStatus = '/accounts/WorkStatus';
 
-  static String preOrderDelivery = '/preorder/delivery';
-  static String preOrderPickUp = '/preorder/pickup';
-  static String orderDelivery = '/order/delivery';
-  static String orderPickUp = '/order/pickup';
-  static String order = '/order';
-  static String voucher = '/Voucher/CheckVoucherValidation';
+  String preOrderDelivery = '/preorder/delivery';
+  String preOrderPickUp = '/preorder/pickup';
+  String orderDelivery = '/order/delivery';
+  String orderPickUp = '/order/pickup';
+  String order = '/order';
+  String voucher = '/Voucher/CheckVoucherValidation';
 
   // get order && get preOrder X(
-  static String preOrderCustomerActive = '/preorder/customer/active';
-  static String preOrderCustomerClosed = '/preorder/customer/closed';
-  static String orderCustomerActive = '/order/customer/active';
-  static String orderCustomerClosed = '/order/customer/closed';
-  static String orderDriverAvailable = '/order/driver/available';
-  static String orderDriverActive = '/order/driver/active';
-  static String orderDriverClosed = '/order/driver/closed';
-  static String preOrderDriverAvailable = '/preorder/driver/available';
-  static String preOrderDriverActive = '/preorder/driver/active';
-  static String preOrderDriverClosed = '/preorder/driver/closed';
-  static String orderDriverAvailableById = '/order/driver/available/';
-  static String orderChefReceived = '/order/chef/received';
-  static String orderChefPreparing = '/order/chef/preparing';
-  static String orderChefReady = '/order/chef/ready';
-  static String orderChefClosed = '/order/chef/closed';
-  static String preOrderChefReceived = '/preorder/chef/received';
-  static String preOrderChefAccepted = '/preorder/chef/accepted';
-  static String preOrderChefPreparing = '/preorder/chef/preparing';
-  static String preOrderChefReady = '/preorder/chef/ready';
-  static String preOrderChefClosed = '/preorder/chef/closed';
+  String preOrderCustomerActive = '/preorder/customer/active';
+  String preOrderCustomerClosed = '/preorder/customer/closed';
+  String orderCustomerActive = '/order/customer/active';
+  String orderCustomerClosed = '/order/customer/closed';
+  String orderDriverAvailable = '/order/driver/available';
+  String orderDriverActive = '/order/driver/active';
+  String orderDriverClosed = '/order/driver/closed';
+  String preOrderDriverAvailable = '/preorder/driver/available';
+  String preOrderDriverActive = '/preorder/driver/active';
+  String preOrderDriverClosed = '/preorder/driver/closed';
+  String orderDriverAvailableById = '/order/driver/available/';
+  String orderChefReceived = '/order/chef/received';
+  String orderChefPreparing = '/order/chef/preparing';
+  String orderChefReady = '/order/chef/ready';
+  String orderChefClosed = '/order/chef/closed';
+  String preOrderChefReceived = '/preorder/chef/received';
+  String preOrderChefAccepted = '/preorder/chef/accepted';
+  String preOrderChefPreparing = '/preorder/chef/preparing';
+  String preOrderChefReady = '/preorder/chef/ready';
+  String preOrderChefClosed = '/preorder/chef/closed';
 
   // action order && preOrder
   /// replace ( _ ) with order id
-  static String orderDriverAccept = '/order/_/driver/accept';
-  static String orderDriverReceived = '/order/_/driver/received';
-  static String orderDriverDelivered = '/order/delivery/driver/delivered';
-  static String preOrderDriverAccept = '/preorder/_/driver/accept';
-  static String preOrderDriverReceived = '/preorder/_/driver/received';
-  static String preOrderDriverDelivered = '/preorder/delivery/driver/delivered';
+  final String _orderDriverAccept = '/order/_/driver/accept';
+  String get orderDriverAccept => getApiKey(_orderDriverAccept);
+  final String _orderDriverReceived = '/order/_/driver/received';
+  String get orderDriverReceived => getApiKey(_orderDriverReceived);
+  String orderDriverDelivered = '/order/delivery/driver/delivered';
+  final String _preOrderDriverAccept = '/preorder/_/driver/accept';
+  String get preOrderDriverAccept => getApiKey(_preOrderDriverAccept);
+  final String _preOrderDriverReceived = '/preorder/_/driver/received';
+  String get preOrderDriverReceived => getApiKey(_preOrderDriverReceived);
+  final String _preOrderDriverDelivered = '/preorder/delivery/driver/delivered';
+  String get preOrderDriverDelivered => getApiKey(_preOrderDriverDelivered);
 
-  static String orderChefDeliveryStart = '/order/_/chef/start';
-  static String orderChefDeliveryFinished = '/order/_/chef/finished';
-  static String orderChefPickUpStart = '/order/_/chef/pickup/start';
-  static String orderChefPickUpFinished = '/order/_/chef/pickup/finished';
-  static String orderChefPickUpDelivered = '/order/pickup/chef/delivered';
+  final String _orderChefDeliveryStart = '/order/_/chef/start';
+  String get orderChefDeliveryStart => getApiKey(_orderChefDeliveryStart);
+  final String _orderChefDeliveryFinished = '/order/_/chef/finished';
+  String get orderChefDeliveryFinished => getApiKey(_orderChefDeliveryFinished);
+  final String _orderChefPickUpStart = '/order/_/chef/pickup/start';
+  String get orderChefPickUpStart => getApiKey(_orderChefPickUpStart);
+  final String _orderChefPickUpFinished = '/order/_/chef/pickup/finished';
+  String get orderChefPickUpFinished => getApiKey(_orderChefPickUpFinished);
+  String orderChefPickUpDelivered = '/order/pickup/chef/delivered';
 
-  static String preOrderChefDeliveryAccept = '/preorder/_/chef/accept';
-  static String preOrderChefDeliveryStart = '/preorder/_/chef/start';
-  static String preOrderChefDeliveryFinished = '/preorder/_/chef/finished';
-  static String preOrderChefPickUpAccept = '/preorder/_/chef/pickup/accept';
-  static String preOrderChefPickUpStart = '/preorder/_/chef/pickup/start';
-  static String preOrderChefPickUpFinished = '/preorder/_/chef/pickup/finished';
-  static String preOrderChefPickUpDelivered = '/preorder/pickup/chef/delivered';
+  final String _preOrderChefDeliveryAccept = '/preorder/_/chef/accept';
+  String get preOrderChefDeliveryAccept =>
+      getApiKey(_preOrderChefDeliveryAccept);
+  final String _preOrderChefDeliveryStart = '/preorder/_/chef/start';
+  String get preOrderChefDeliveryStart => getApiKey(_preOrderChefDeliveryStart);
+  final String _preOrderChefDeliveryFinished = '/preorder/_/chef/finished';
+  String get preOrderChefDeliveryFinished =>
+      getApiKey(_preOrderChefDeliveryFinished);
+  final String _preOrderChefPickUpAccept = '/preorder/_/chef/pickup/accept';
+  String get preOrderChefPickUpAccept => getApiKey(_preOrderChefPickUpAccept);
+  final String _preOrderChefPickUpStart = '/preorder/_/chef/pickup/start';
+  String get preOrderChefPickUpStart => getApiKey(_preOrderChefPickUpStart);
+  final String _preOrderChefPickUpFinished = '/preorder/_/chef/pickup/finished';
+  String get preOrderChefPickUpFinished =>
+      getApiKey(_preOrderChefPickUpFinished);
+  String preOrderChefPickUpDelivered = '/preorder/pickup/chef/delivered';
 
   // customers
-  static String waitChefOrder = '/order/wait/chef';
-  static String cancelChefOrder = '/order/cancel/chef';
-  static String waitDriverOrder = '/order/wait/driver';
-  static String cancelDriverOrder = '/order/cancel/driver';
+  String waitChefOrder = '/order/wait/chef';
+  String cancelChefOrder = '/order/cancel/chef';
+  String waitDriverOrder = '/order/wait/driver';
+  String cancelDriverOrder = '/order/cancel/driver';
 
   // chef
-  static String preorderCancelChefDelivery = '/preorder/cancel/chef/delivery';
-  static String preorderCancelChefPickup = '/preorder/cancel/chef/pickup';
+  String preorderCancelChefDelivery = '/preorder/cancel/chef/delivery';
+  String preorderCancelChefPickup = '/preorder/cancel/chef/pickup';
 
-  static String updateInvoice = '/api/Invoices/UpdateRestaurantInvoice/_/48';
+  final String _updateInvoice = '/api/Invoices/UpdateRestaurantInvoice/_/48';
+  String get updateInvoice => getApiKey(_updateInvoice);
 
-  static String review = '/accounts/review';
+  String review = '/accounts/review';
 }
