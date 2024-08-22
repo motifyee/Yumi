23f8b306248df1ce29ca5870f76591eaef7fa780
mrt <iquixe@gmail.com>
2024-08-22 10:48:49 +0300

    add testcases

--- test/data/sources/schedule_remote_src_test.dart
+++ test/data/sources/schedule_remote_src_test.dart
@@ -1,14 +1,95 @@
 import 'package:common_code/common_code.dart';
+import 'package:common_code/domain/profile/data/sources/remote/profile_remote_src.dart';
 import 'package:dependencies/dependencies.dart';
+import 'package:flutter_test/flutter_test.dart' as ftest;
 import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
 import 'package:common_code/domain/schedule/data/sources/remote/schedule_remote_source.dart';
 
-// TODO test all the rules of data sources for all data sources in a single test
-
-class MockClient extends Mock implements APIClient {}
+import 'testcases.dart';
 
 void main() {
+  ftest.TestWidgetsFlutterBinding.ensureInitialized();
+
+  runWhen();
+
+  group('Test All Sources', () {
+    for (var testCase in testCases) {
+      group('${testCase.src.toString()}: Should return', () {
+        test(
+            '${testCase.entityType.toString()} when response is 200 and has valid data',
+            () async {
+          final value = Future.value(
+            Response<Map<String, dynamic>>(
+              data: testCase.entity ??
+                  (
+                    testCase.entityType == String
+                        ? "string"
+                        : (testCase.entityType is List ? [] : {}),
+                  ),
+              statusCode: 200,
+              requestOptions: RequestOptions(),
+            ),
+          );
+          when(() => mockClient.get(testCase.url)).thenAnswer((ans) => value);
+          when(() => mockClient.post(testCase.url)).thenAnswer((ans) => value);
+          when(() => mockClient.put(testCase.url)).thenAnswer((ans) => value);
+          when(() => mockClient.delete(testCase.url))
+              .thenAnswer((ans) => value);
+
+          expect(await testCase.call(), isNotNull);
+        });
+      });
+
+      group('${testCase.src.toString()}: Should throw', () {
+        test(
+            'a ServerException when DioException\'s response is not 2xx or 304',
+            () async {
+          when(() => mockClient.get(testCase.url)).thenAnswer(
+            (ans) => throw DioException(
+              requestOptions: RequestOptions(),
+              response: Response(requestOptions: RequestOptions()),
+            ),
+          );
+
+          expect(
+            testCase.call(),
+            throwsA(isA<ServerException>()),
+          );
+        });
+
+        test('a NetworkException when DioException has no response', () async {
+          when(() => mockClient.get(testCase.url)).thenAnswer(
+            (ans) => throw DioException(
+              requestOptions: RequestOptions(),
+            ),
+          );
+
+          expect(
+            testCase.call(),
+            throwsA(isA<NetworkException>()),
+          );
+        });
+
+        test('a TypeError when response is 200 and has invalid data', () async {
+          when(() => mockClient.get(testCase.url)).thenAnswer(
+            (ans) => Future.value(
+              Response(
+                data: '',
+                statusCode: 200,
+                requestOptions: RequestOptions(),
+              ),
+            ),
+          );
+
+          expect(
+            testCase.call(),
+            throwsA(isA<TypeError>()),
+          );
+        });
+      });
+    }
+  });
   group('ScheduleSrc', () {
     final mockClient = MockClient();
     final scheduleSrcTest = ScheduleRemoteSrc(client: mockClient);
@@ -17,7 +98,7 @@ void main() {
       test('when response is 200 and has valid data', () async {
         when(() => mockClient.get('/accounts/schedule')).thenAnswer(
           (ans) => Future.value(
-            Response(
+            Response<Map<String, dynamic>>(
               data: {},
               statusCode: 200,
               requestOptions: RequestOptions(),
@@ -31,20 +112,31 @@ void main() {
     });
 
     group('Should throw', () {
-      test('a ServerException when response is not 200', () async {
+      test('a ServerException when DioException\'s response is not 2xx or 304',
+          () async {
         when(() => mockClient.get('/accounts/schedule')).thenAnswer(
-          (ans) => Future.value(
-            Response(
-              data: null,
-              statusCode: 400,
-              requestOptions: RequestOptions(),
-            ),
+          (ans) => throw DioException(
+            requestOptions: RequestOptions(),
+            response: Response(requestOptions: RequestOptions()),
           ),
         );
 
         expect(
           () => scheduleSrcTest.getMySchedule(),
           throwsA(isA<ServerException>()),
+        );
+      });
+
+      test('a NetworkException when DioException has no response', () async {
+        when(() => mockClient.get('/accounts/schedule')).thenAnswer(
+          (ans) => throw DioException(
+            requestOptions: RequestOptions(),
+          ),
+        );
+
+        expect(
+          () => scheduleSrcTest.getMySchedule(),
+          throwsA(isA<NetworkException>()),
         );
       });
 
@@ -61,6 +153,75 @@ void main() {
 
         expect(
           () => scheduleSrcTest.getMySchedule(),
+          throwsA(isA<TypeError>()),
+        );
+      });
+    });
+  });
+
+  group('ProfileSrc', () {
+    final mockClient = MockClient();
+    final scheduleSrcTest = ProfileRemoteSrc(client: mockClient);
+
+    group('Should return Profile', () {
+      test('when response is 200 and has valid data', () async {
+        when(() => mockClient.get('chefs/abc')).thenAnswer(
+          (ans) => Future.value(
+            Response<Map<String, dynamic>>(
+              data: {},
+              statusCode: 200,
+              requestOptions: RequestOptions(),
+            ),
+          ),
+        );
+
+        final res = await scheduleSrcTest.loadProfile('abc', 'any@email.com');
+        expect(res, isNotNull);
+      });
+    });
+
+    group('Should throw', () {
+      test('a ServerException when DioException\'s response is not 2xx or 304',
+          () async {
+        when(() => mockClient.get('/accounts/schedule')).thenAnswer(
+          (ans) => throw DioException(
+            requestOptions: RequestOptions(),
+            response: Response(requestOptions: RequestOptions()),
+          ),
+        );
+
+        expect(
+          () => scheduleSrcTest.loadProfile('abc', 'any@email.com'),
+          throwsA(isA<ServerException>()),
+        );
+      });
+
+      test('a NetworkException when DioException has no response', () async {
+        when(() => mockClient.get('/accounts/schedule')).thenAnswer(
+          (ans) => throw DioException(
+            requestOptions: RequestOptions(),
+          ),
+        );
+
+        expect(
+          () => scheduleSrcTest.loadProfile('abc', 'any@email.com'),
+          throwsA(isA<NetworkException>()),
+        );
+      });
+
+      test('a TypeError when response is 200 and has invalid data', () async {
+        when(() => mockClient.get('/accounts/schedule')).thenAnswer(
+          (ans) => Future.value(
+            Response(
+              data: '',
+              statusCode: 200,
+              requestOptions: RequestOptions(),
+            ),
+          ),
+        );
+
+        expect(
+          () => scheduleSrcTest.loadProfile('abc', 'any@email.com'),
           throwsA(isA<TypeError>()),
         );
       });
