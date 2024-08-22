81b90bd3114c6d59754ca4bf487b4c943c4595bf
mrt <iquixe@gmail.com>
2024-08-22 10:48:28 +0300

    schedule_remote_src: test fixes

--- common_code/lib/domain/schedule/data/sources/remote/schedule_remote_source.dart
+++ common_code/lib/domain/schedule/data/sources/remote/schedule_remote_source.dart
@@ -1,4 +1,4 @@
-import 'package:dio/dio.dart';
+import 'package:dependencies/dependencies.dart';
 import 'package:common_code/domain/schedule/data/sources/schedule_sources.dart';
 import 'package:common_code/domain/schedule/entities/schedule.dart';
 import 'package:common_code/common_code.dart';
@@ -10,18 +10,18 @@ class ScheduleRemoteSrc implements ScheduleSrc {
 
   @override
   Future<Schedule> getMySchedule() async {
-    final Response res = await client.get(
-      '/accounts/schedule',
-    );
+    final Response res;
+    try {
+      res = await client.get(
+        '/accounts/schedule',
+      );
+    } catch (e) {
+      throw e.exceptionFromDio;
+    }
 
     if (res.statusCode != 200 && res.statusCode != 201) throw ServerException();
 
-    var data = <String, dynamic>{};
-
-    if (res.data.toString().isNotEmpty) data = res.data;
-
-    var fromJson = Schedule.fromJson(data);
-    return fromJson;
+    return Schedule.fromJson(res.data);
   }
 
   @override
@@ -43,10 +43,15 @@ class ScheduleRemoteSrc implements ScheduleSrc {
 
     // -------------------------------------------------------------------------
 
-    final Response res = await client.put(
-      '/accounts/schedule',
-      data: data,
-    );
+    final Response res;
+    try {
+      res = await client.put(
+        '/accounts/schedule',
+        data: data,
+      );
+    } catch (e) {
+      throw e.exceptionFromDio;
+    }
 
     if (res.statusCode != 200) throw ServerException();
 
